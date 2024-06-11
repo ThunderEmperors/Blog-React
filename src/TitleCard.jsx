fb2170584f17f52c312866500919ff479bfa570e
ThunderEmperors <puranjayjoshi@gmail.com>
2024-06-12 02:43:08 +0530

    Removed Original JSX added Basic Navbar and TitleCard

---
+++ src/TitleCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './TitleCard.css';
+
+const TitleCard = () => {
+    return(
+<card className="Card">
+        <div className="MainTitle">
+            ASD
+        </div>
+        <div className="Description">
+            SDF
+        </div>
+        <div className="Utility">
+            
+        </div>
+</card>
+    )
+}
+
+export default TitleCard;
