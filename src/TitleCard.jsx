9b29660e63cdf4963180b5b51b0519e73819e975
ThunderEmperors <puranjayjoshi@gmail.com>
2024-06-12 17:34:47 +0530

    added basic input fields for title and desc

--- src/TitleCard.jsx
+++ src/TitleCard.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './TitleCard.css';
 
 const TitleCard = () => {
+
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const postData = () => {
+        console.log(title);
+        console.log(desc);
+    }
+
+
     return(
 <card className="Card">
         <div className="MainTitle">
-            ASD
+           Title
         </div>
         <div className="Description">
-            SDF
+            Description
         </div>
         <div className="Utility">
-            
+            <input placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+            <input placeholder="Description" onChange={(e) => setDesc(e.target.value)}/>
+            <button onClick={postData}> Submit </button>
         </div>
 </card>
     )
